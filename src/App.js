c5dceb2d47e5e3eb65bbb541e6186f7fd19329eb
rsagar <rsagar@amps.com>
2025-07-29 15:28:39 +0530

    production config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [originalUrl, setOriginalUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/shorten`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ originalUrl: originalUrl })
+    });
+
+    const short = await res.text();
+    setShortUrl(short);
+  };
+
+  const handleRedirect = async (shortUrl) => {
+    try {
+      const response = await fetch(shortUrl, { method: "GET", redirect: "manual" });
+
+      console.log("Response status:", response);
+      if (response.status === 0) {
+        const location = response.url;
+        window.open(location, "_blank");
+      } else
+        if (response.status === 302) {
+          const location = response.url;
+          console.log("Redirect to:", location);
+          window.location.href = location;
+        } else if (response.status === 410) {
+          alert("This link has expired. Please Generate Again");
+        } else if (response.status === 404) {
+          alert("URL does not exist.");
+          window.location.href = 'http://localhost:3000';
+        } else {
+          alert("Unexpected response");
+        }
+    } catch (error) {
+      console.error("Redirect failed:", error);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <form onSubmit={handleSubmit} style={{ textAlign: 'center', marginTop: '10%' }}>
+        <label>Enter the Link : </label>
+        <input value={originalUrl} onChange={(e) => setOriginalUrl(e.target.value)} placeholder="Enter URL" />
+        <button type="submit">Shorten</button>
+      </form>
+      {shortUrl && (
+        <p style={{ textAlign: 'center', marginTop: '2%' }}>
+          Short URL:{" "}
+          <button onClick={() => handleRedirect(shortUrl)} style={{ cursor: "pointer", color: "blue", textDecoration: "underline", background: "none", border: "none" }}>
+            {shortUrl}
+          </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
